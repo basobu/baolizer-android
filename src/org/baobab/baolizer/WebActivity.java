7715edcc724aa81469196738248efff9b2d883ea
orangeman <flo@teleportr.org>
2013-11-29 02:32:08 +0100

    webview loading indicator

--- src/org/baobab/baolizer/WebActivity.java
+++ src/org/baobab/baolizer/WebActivity.java
@@ -2,20 +2,20 @@ package org.baobab.baolizer;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.Window;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
 import android.webkit.JsResult;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.ProgressBar;
+import android.widget.ImageView;
 
 import de.keyboardsurfer.android.widget.crouton.Crouton;
 import de.keyboardsurfer.android.widget.crouton.Style;
@@ -25,19 +25,20 @@ import de.keyboardsurfer.android.widget.crouton.Style;
 public class WebActivity extends Activity {
 
     private static final String TAG = "WebActivity";
-    private ProgressBar progress;
+    private ImageView progress;
     private WebView webView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-        webView = new WebView(this);
-        progress = new ProgressDialog(this);
+        setContentView(R.layout.activity_web);
+        progress = (ImageView) findViewById(R.id.progress);
+        webView = (WebView) findViewById(R.id.webview);
         webView.getSettings().setJavaScriptEnabled(true);
         webView.getSettings().setSaveFormData(false);
         webView.getSettings().setSavePassword(false);
-        webView.setWebChromeClient(new WebChromeClient(){
+        webView.setWebChromeClient(new WebChromeClient() {
             @Override
             public boolean onJsAlert(WebView view, String url, String message,
                     JsResult result) {
@@ -47,19 +48,26 @@ public class WebActivity extends Activity {
             }
         });
         webView.setWebViewClient(new WebViewClient() {
-            
+
             @Override
             public void onPageStarted(WebView v, String url, Bitmap favic) {
-                if (url.startsWith("http")) {
-                    progress.show();
-                }
                 super.onPageStarted(v, url, favic);
+                if (url.startsWith("http")) {
+                    progress.setVisibility(View.VISIBLE);
+                    RotateAnimation rotate = new RotateAnimation(
+                            0f, 360f, Animation.RELATIVE_TO_SELF,
+                            0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+                    rotate.setDuration(600);
+                    rotate.setRepeatMode(Animation.RESTART);
+                    rotate.setRepeatCount(Animation.INFINITE);
+                    progress.startAnimation(rotate);
+                }
             }
 
             @Override
             public void onPageFinished(WebView view, String url) {
-                progress.dismiss();
                 super.onPageFinished(view, url);
+                progress.setVisibility(View.GONE);
             }
 
             @Override
@@ -73,7 +81,6 @@ public class WebActivity extends Activity {
         });
         webView.loadUrl(getIntent().getDataString());
         webView.requestFocus(View.FOCUS_DOWN);
-        setContentView(webView);
     }
 
     @Override
