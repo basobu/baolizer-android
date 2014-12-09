c3b7a54665c122d0e2916516d2d70934b9f06a0d
orangeman <flo@teleportr.org>
2014-12-09 21:13:13 +0100

    fb only if fb is

--- src/main/java/org/baobab/baolizer/ProfileActivity.java
+++ src/main/java/org/baobab/baolizer/ProfileActivity.java
@@ -1,8 +1,11 @@
 package org.baobab.baolizer;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.util.Log;
@@ -19,6 +22,7 @@ import com.facebook.model.GraphUser;
 import com.facebook.widget.LoginButton;
 
 import java.util.Arrays;
+import java.util.List;
 
 public class ProfileActivity extends Activity implements Session.StatusCallback {
 
@@ -31,23 +35,29 @@ public class ProfileActivity extends Activity implements Session.StatusCallback 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
         wheel = (ProgressBar) findViewById(R.id.wheel);
-        fbButton = (LoginButton) findViewById(R.id.fbButton);
-        fbButton.setReadPermissions(Arrays.asList("email"));
-        fbHelper = new UiLifecycleHelper(this, this);
-        fbHelper.onCreate(savedInstanceState);
+        if (isFacebook(this)) {
+            fbButton = (LoginButton) findViewById(R.id.fbButton);
+            fbButton.setReadPermissions(Arrays.asList("email"));
+            fbHelper = new UiLifecycleHelper(this, this);
+            fbHelper.onCreate(savedInstanceState);
+        } else {
+            loggedIn("Profil", "email@internet.com");
+        }
     }
 
     @Override
     public void onResume() {
         super.onResume();
-        Session session = Session.getActiveSession();
-        if (session != null && (session.isOpened() || session.isClosed()) ) {
-            call(session, session.getState(), null);
-        } else {
-            fbButton.setVisibility(View.VISIBLE);
-            wheel.setVisibility(View.GONE);
+        if (fbHelper != null) {
+            Session session = Session.getActiveSession();
+            if (session != null && (session.isOpened() || session.isClosed()) ) {
+                call(session, session.getState(), null);
+            } else {
+                fbButton.setVisibility(View.VISIBLE);
+                wheel.setVisibility(View.GONE);
+            }
+            fbHelper.onResume();
         }
-        fbHelper.onResume();
     }
 
     @Override
@@ -89,31 +99,59 @@ public class ProfileActivity extends Activity implements Session.StatusCallback 
     private void loggedIn(String name, String email) {
         setTitle(name);
         ((TextView) findViewById(R.id.email)).setText(email);
-        fbButton.setVisibility(View.VISIBLE);
+        if (fbButton != null) {
+            fbButton.setVisibility(View.VISIBLE);
+        }
         wheel.setVisibility(View.GONE);
     }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        fbHelper.onActivityResult(requestCode, resultCode, data);
+        if (fbHelper != null) {
+            fbHelper.onActivityResult(requestCode, resultCode, data);
+        }
     }
 
     @Override
     public void onPause() {
         super.onPause();
-        fbHelper.onPause();
+        if (fbHelper != null) {
+            fbHelper.onPause();
+        }
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-        fbHelper.onDestroy();
+        if (fbHelper != null) {
+            fbHelper.onDestroy();
+        }
     }
 
     @Override
     public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-        fbHelper.onSaveInstanceState(outState);
+        if (fbHelper != null) {
+            fbHelper.onSaveInstanceState(outState);
+        }
+    }
+
+    public static boolean isFacebook(Context ctx) {
+        final String[] name = {"com.facebook.android", "com.facebook.katana"};
+        Intent intent = new Intent();
+        intent.setType("text/plain");
+        final PackageManager packageManager = ctx.getPackageManager();
+        List<ResolveInfo> list = packageManager.queryIntentActivities(
+                intent, PackageManager.MATCH_DEFAULT_ONLY);
+        for (int i = 0; i < name.length; i++) {
+            for (ResolveInfo resolveInfo : list) {
+                String p = resolveInfo.activityInfo.packageName;
+                if (p != null && p.startsWith(name[i])) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
