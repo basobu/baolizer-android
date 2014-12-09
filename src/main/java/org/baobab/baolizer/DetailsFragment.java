662dad356dfad9e652a91987bf0c30e84aa7adb1
orangeman <flo@teleportr.org>
2014-12-09 20:54:28 +0100

    load products in baobab details

--- src/main/java/org/baobab/baolizer/DetailsFragment.java
+++ src/main/java/org/baobab/baolizer/DetailsFragment.java
@@ -1,13 +1,24 @@
 package org.baobab.baolizer;
 
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ListFragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+import android.support.v4.widget.CursorAdapter;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
-public class DetailsFragment extends Fragment {
+public class DetailsFragment extends ListFragment
+        implements LoaderManager.LoaderCallbacks<Cursor> {
 
     public static final String WEBVIEW = "webview/";
     private static final String PAGE_HTML = ".page.html";
@@ -27,9 +38,46 @@ public class DetailsFragment extends Fragment {
         feedback = (ImageButton) frame.findViewById(R.id.feedback);
         String id = getArguments().getString("id");
         ((WebFragment) getChildFragmentManager()
-                .findFragmentById(R.id.products))
+                .findFragmentById(R.id.web))
                 .load(RefreshService.BASE_URL +
                         WEBVIEW + id + PAGE_HTML);
+        getLoaderManager().initLoader(Integer.parseInt(id), null, this);
         return frame;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        setListAdapter(new CursorAdapter(getActivity(), null, true) {
+            @Override
+            public View newView(Context context, Cursor cursor, ViewGroup parent) {
+                return new TextView(getActivity());
+            }
+
+            @Override
+            public void bindView(View view, Context context, Cursor cursor) {
+                ((TextView) view).setText(cursor.getString(1));
+                ((TextView) view).setTextSize(23);
+            }
+        });
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
+        return new CursorLoader(getActivity(),
+                Uri.parse("content://org.baobab.baolizer/baobabs/" +
+                getArguments().getString("id") + "/products"),
+                null, null, null, null);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+        Log.d("Baolizer", "loaded " + cursor.getCount());
+        ((CursorAdapter) getListAdapter()).swapCursor(cursor);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        ((CursorAdapter) getListAdapter()).swapCursor(null);
     }
 }
